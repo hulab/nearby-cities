e258373bc796b8e4249938d7faa6cbdd5ff34568
SebC99 <sebastien.caron@gmail.com>
2021-06-24 17:28:50 +0200

    add distance in km in results

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const geokdbush = require('geokdbush')
 
 const index = kdbush(cities, (p) => p.loc.coordinates[0], (p) => p.loc.coordinates[1])
 
-module.exports = function (input, maxResults, maxDistance) {
-  const lon = input.coords ? +input.coords.longitude : +input.longitude
-  const lat = input.coords ? +input.coords.latitude : +input.latitude
+module.exports = function ({longitude, latitude}, maxResults = 5, maxDistance) {
+  const lon = +longitude;
+  const lat = +latitude;
 
   assert(!isNaN(lon) && !isNaN(lat), 'function requires an object with `latitude` and `longitude` properties')
 
   return geokdbush.around(index, lon, lat, maxResults, maxDistance)
+    .map((result) => Object.assign(result, {distance: geokdbush.distance(lon, lat, result.loc.coordinates[0], result.loc.coordinates[1])}));
 }
